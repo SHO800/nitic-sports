7c05c140f48ef2dd8307f9928f37d8d09b1f70c8
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-05-01 18:14:58 +0900

    カードのフォントを調整

--- src/app/match/[locationId]/page.tsx
+++ src/app/match/[locationId]/page.tsx
@@ -6,12 +6,12 @@ import MatchesByLocation from "@/components/match/MatchesByLocation";
 const MatchDashboard = () => {
     const params = useParams<{ locationId: string }>()
     return (
-        <div className={"flex flex-row w-full h-full"}>
+        <div className={"flex flex-row w-screen h-[calc(100vh-129px)] overflow-y-scroll"}>
         <MatchesByLocation locationId={"1"}/>
         <MatchesByLocation locationId={"2"}/>
         <MatchesByLocation locationId={"3"}/>
-        {/*<MatchesByLocation locationId={"4"}/>*/}
-        {/*<MatchesByLocation locationId={"5"}/>*/}
+        <MatchesByLocation locationId={"4"}/>
+        <MatchesByLocation locationId={"5"}/>
         </div>
     )
 }
--- src/components/match/MatchesByLocation.tsx
+++ src/components/match/MatchesByLocation.tsx
@@ -1,11 +1,29 @@
 import {useData} from "@/hooks/data";
-import {Fragment, useEffect, useMemo, useState} from "react";
+import {Fragment, useEffect, useMemo, useRef, useState} from "react";
 import {Event, Location, MatchPlan} from "@prisma/client";
 
 const MatchesByLocation = ({locationId}: { locationId: number }) => {
     const {matchPlans, locations, events, getMatchDisplayStr} = useData()
     const [currentLocation, setCurrentLocation] = useState<Location | null>(null)
     const [matchesInLocation, setMatchesInLocation] = useState<MatchPlan[]>([])
+    const wrapperRef = useRef<HTMLDivElement | null>(null);
+    const [fontSize, setFontSize] = useState(16);
+    
+    const handleResize = () => {
+        if (wrapperRef.current) {
+            const width = wrapperRef.current.clientWidth;
+            const newFontSize = Math.max(16, Math.floor(width / 20)); // Adjust the divisor to control the scaling
+            setFontSize(newFontSize);
+        }
+    }
+    
+    useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [wrapperRef]);
 
     const eventsById = useMemo(() => {
         if (!events) return {}
@@ -35,27 +53,41 @@ const MatchesByLocation = ({locationId}: { locationId: number }) => {
     }, [locations, locationId])
 
     return (
-        <div className={"w-full h-full flex flex-col items-center"}>
+        <div className={"w-full h-full flex flex-col items-center"} style={{fontSize: fontSize+"px"}} ref={wrapperRef}>
             <div
                 className={"ml-8 pr-8 mt-5 pb-2 w-full text-4xl font-bold border-b-2 [border-image:linear-gradient(to_right,#888,#888_60%,#eee)_1]"}>
                 {currentLocation?.name}
             </div>
 
-            <div className={"flex flex-col items-center m-8 space-y-4 w-full overflow-x-scroll overscroll-none"}>
+            <div className={"flex flex-col items-center px-4 m-8 space-y-4 w-full overflow-x-scroll overscroll-none"}>
                 {matchesInLocation.map(match => {
                     const teamsDisplayNames = match.teamIds.map(teamId => getMatchDisplayStr(teamId))
 
                     return (
                         // card
                         <div key={"matches-" + match.id}
-                             // className={"h-[min(70vh,70vw)] aspect-[0.7071067812] px-4 py-2  border border-gray-500 rounded text-[min(4vh,4vw)]"}
-                             className={" px-4 py-2  border border-gray-500 rounded "}
+                             className={"w-full px-4 py-2  border border-gray-500 rounded "}
                         >
+                            {/*ヘッダ*/}
                             <div className={"flex flex-row justify-between space-x-4 w-full "}>
                                 <p className={"font-bold m-0"}>{match.matchName} <small
                                     className={"text-gray-500 ml-1 font-normal"}>#{match.id}</small></p>
                                 <p className={"text-[.9em] mt-auto mb-1 w-fit whitespace-nowrap min-w-16  "}>{eventsById[match.eventId]?.name}</p>
                             </div>
+                            
+                            {/*開始時間等*/}
+                            <p className={"text-[.9em] mt-auto mb-1 w-fit whitespace-nowrap min-w-16  "}>
+                                {new Date(match.scheduledStartTime).toLocaleString("ja-JP", {
+                                    year: "numeric",
+                                    month: "2-digit",
+                                    day: "2-digit",
+                                    hour: "2-digit",
+                                    minute: "2-digit",
+                                    second: "2-digit",
+                                })}
+                            </p>
+                            
+                            {/*対戦チーム等*/}
                             <p className={"flex flex-row justify-center space-x-8 border-y-[2px] border-y-gray-400"}>
                                 {teamsDisplayNames.map((teamName, index) => {
                                     return (
@@ -67,8 +99,9 @@ const MatchesByLocation = ({locationId}: { locationId: number }) => {
                                     )
                                 })}
                             </p>
+                            
+                            {/*    操作ボタン*/}
                             <div className={"flex flex-row justify-start"}>
-
 
                             </div>
                         </div>
