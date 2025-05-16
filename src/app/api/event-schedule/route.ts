b5fa03d6e38cf92da0d62a6427ada7c617be06b0
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-05-16 14:38:48 +0900

    使用しないevent-scheduleテーブル用のapiルートを削除

--- src/app/api/event-schedule/route.ts
+++
@@ -1,8 +0,0 @@
-import {prisma} from "../../../../lib/prisma";
-import {EventSchedule} from "@prisma/client";
-
-export async function GET() {
-    const eventSchedules: EventSchedule[] = await prisma.eventSchedule.findMany()
-    return Response.json(eventSchedules)
-    
-}
