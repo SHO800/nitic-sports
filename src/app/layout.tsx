ed20e5a6c597de5942915c2ddba7f0690b26242f
SHO800 <fg582323@docomo.ne.jp>
2025-05-18 20:07:29 +0900

    ヘッダーに時計を表示

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,50 +1,50 @@
 "use cache";
-import type { Metadata } from "next";
-import { Noto_Sans_JP } from "next/font/google";
+import type {Metadata} from "next";
+import {Noto_Sans_JP} from "next/font/google";
 import "./globals.css";
 import DataPreFetcher from "@/components/common/DataPreFetcher";
 import Footer from "@/components/layout/Footer/page";
 import Header from "@/components/layout/Header/page";
-import { CurrentTimeContextProvider } from "@/contexts/currentTimeContext";
-import { DataContextProvider } from "@/contexts/dataContext";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import {CurrentTimeContextProvider} from "@/contexts/currentTimeContext";
+import {DataContextProvider} from "@/contexts/dataContext";
+import {Analytics} from "@vercel/analytics/react";
+import {SpeedInsights} from "@vercel/speed-insights/next";
 
 const notoSansJp = Noto_Sans_JP({
-	subsets: ["latin"],
-	weight: ["400", "700"],
-	preload: false,
-	variable: "--font-noto-sans-jp",
-	display: "swap",
-	fallback: ["Hiragino Sans", "Hiragino Kaku Gothic ProN", "sans-serif"],
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    preload: false,
+    variable: "--font-noto-sans-jp",
+    display: "swap",
+    fallback: ["Hiragino Sans", "Hiragino Kaku Gothic ProN", "sans-serif"],
 });
 
 export const metadata: Metadata = {
-	title: "一関 - 体育大会2025",
-	description: "一関高専校内体育大会の試合結果をリアルタイムでお届けします！",
+    title: "一関 - 体育大会2025",
+    description: "一関高専校内体育大会の試合結果をリアルタイムでお届けします！",
 };
 
 export default async function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
+                                             children,
+                                         }: Readonly<{
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${notoSansJp.variable} antialiased text-lg bg-gray-200`}
-			>
-				<Header />
-				<CurrentTimeContextProvider>
-					<DataPreFetcher>
-						<DataContextProvider>{children}</DataContextProvider>
-					</DataPreFetcher>
-				</CurrentTimeContextProvider>
+    return (
+        <html lang="en">
+        <body
+            className={`${notoSansJp.variable} antialiased text-lg bg-gray-200`}
+        >
+        <CurrentTimeContextProvider>
+            <Header/>
+            <DataPreFetcher>
+                <DataContextProvider>{children}</DataContextProvider>
+            </DataPreFetcher>
+        </CurrentTimeContextProvider>
 
-				<Analytics />
-				<SpeedInsights />
-				<Footer />
-			</body>
-		</html>
-	);
+        <Analytics/>
+        <SpeedInsights/>
+        <Footer/>
+        </body>
+        </html>
+    );
 }
--- src/components/layout/Header/page.tsx
+++ src/components/layout/Header/page.tsx
@@ -1,107 +1,114 @@
 "use client";
 
-import { Transition } from "@headlessui/react";
+import {Transition} from "@headlessui/react";
 import clsx from "clsx";
 import Link from "next/link";
-import { useState } from "react";
+import {useState} from "react";
+import Clock from "@/components/common/Clock";
 
 export default function Header() {
-	const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<>
-			<nav className="fixed z-100 w-full bg-blue-900 text-white p-4 rounded">
-				<div className="flex items-center justify-between">
-					<Link href="/" onClick={() => setIsOpen(!isOpen)} className="text-lg font-bold">
-						2025年度体育大会
-					</Link>
+    return (
+        <>
+            <nav className="fixed z-100 w-full bg-blue-900 text-white p-4 rounded-b-xl">
+                <div className="flex items-center justify-between">
+                    <Link href="/" onClick={() => setIsOpen(!isOpen)} className="text-lg font-bold">
+                        2025年度体育大会
+                    </Link>
 
-					{/* ハンバーガーアイコン */}
-					<button
-						onClick={() => setIsOpen(!isOpen)}
-						className="md:hidden relative flex w-10 h-10 justify-center items-center"
-						aria-label="Toggle menu"
-					>
+                    <div className={"scale-50 lg:fixed lg:top-1 lg:left-1/2 lg:-translate-x-1/2 "}>
+                        <Clock/>
+                    </div>
+
+
+                    {/* ハンバーガーアイコン */}
+                    <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="md:hidden relative flex w-10 h-10 justify-center items-center"
+                        aria-label="Toggle menu"
+                    >
 						<span
-							className={clsx(
-								"absolute w-6 h-0.5 bg-white transition-transform duration-300",
-								isOpen ? "rotate-45" : "top-2.5",
-							)}
-						/>
-						<span
-							className={clsx(
-								"absolute w-6 h-0.5 bg-white transition-all duration-300",
-								isOpen ? "opacity-0" : "",
-							)}
-						/>
-						<span
-							className={clsx(
-								"absolute w-6 h-0.5 bg-white transition-transform duration-300",
-								isOpen ? "-rotate-45" : "bottom-2.5",
-							)}
-						/>
-					</button>
+                            className={clsx(
+                                "absolute w-6 h-0.5 bg-white transition-transform duration-300",
+                                isOpen ? "rotate-45" : "top-2.5",
+                            )}
+                        />
+                        <span
+                            className={clsx(
+                                "absolute w-6 h-0.5 bg-white transition-all duration-300",
+                                isOpen ? "opacity-0" : "",
+                            )}
+                        />
+                        <span
+                            className={clsx(
+                                "absolute w-6 h-0.5 bg-white transition-transform duration-300",
+                                isOpen ? "-rotate-45" : "bottom-2.5",
+                            )}
+                        />
+                    </button>
 
-					{/* メニュー（PC用） */}
-					<div className="hidden md:flex gap-4">
-						<Link href="/">Home</Link>
-						<Link href="/dashboard">Dashboard</Link>
-						<Link href="/schedule">Schedule</Link>
-						<Link href="/infomation">Infomation</Link>
-						<Link href="/map">Map</Link>
-					</div>
-				</div>
+                    {/* メニュー（PC用） */}
+                    <div className="hidden md:flex gap-4">
+                        <Link href="/">Home</Link>
+                        <Link href="/dashboard">Dashboard</Link>
+                        <Link href="/schedule">Schedule</Link>
+                        <Link href="/infomation">Infomation</Link>
+                        <Link href="/map">Map</Link>
+                    </div>
+                </div>
 
-				{/* メニュー（モバイル用） */}
-				<Transition
-					show={isOpen}
-					enter="transition-transform duration-300"
-					enterFrom="-translate-x-full"
-					enterTo="translate-x-0"
-					leave="transition-transform duration-300"
-					leaveFrom="translate-x-0"
-					leaveTo="-translate-x-full"
-				>
-					<div className="md:hidden relative z-50 bg-gray-200 text-black mt-2 p-4 flex flex-col gap-4 transform rounded">
-						<Link href="/" onClick={() => setIsOpen(!isOpen)} className="pl-2">
-							Home
-						</Link>
-						<div className="h-0.5 bg-gray-400" />
-						<Link
-							href="/dashboard"
-							onClick={() => setIsOpen(!isOpen)}
-							className="pl-2"
-						>
-							Dashboard
-						</Link>
-						<div className="h-0.5 bg-gray-400" />
-						<Link
-							href="/schedule"
-							onClick={() => setIsOpen(!isOpen)}
-							className="pl-2"
-						>
-							Schedule
-						</Link>
-						<div className="h-0.5 bg-gray-400" />
-						<Link
-							href="/infomation"
-							onClick={() => setIsOpen(!isOpen)}
-							className="pl-2"
-						>
-							Infomation
-						</Link>
-						<div className="h-0.5 bg-gray-400" />
-						<Link
-							href="/map"
-							onClick={() => setIsOpen(!isOpen)}
-							className="pl-2"
-						>
-							Map
-						</Link>
-					</div>
-				</Transition>
-			</nav>
-			<div className="w-full p-6 rounded">blank</div>
-		</>
-	);
+                {/* メニュー（モバイル用） */}
+                <Transition
+                    show={isOpen}
+                    enter="transition-transform duration-300"
+                    enterFrom="-translate-x-full"
+                    enterTo="translate-x-0"
+                    leave="transition-transform duration-300"
+                    leaveFrom="translate-x-0"
+                    leaveTo="-translate-x-full"
+                >
+                    <div
+                        className="md:hidden relative z-50 bg-gray-200 text-black mt-2 p-4 flex flex-col gap-4 transform rounded">
+                        <Link href="/" onClick={() => setIsOpen(!isOpen)} className="pl-2">
+                            Home
+                        </Link>
+                        <div className="h-0.5 bg-gray-400"/>
+                        <Link
+                            href="/dashboard"
+                            onClick={() => setIsOpen(!isOpen)}
+                            className="pl-2"
+                        >
+                            Dashboard
+                        </Link>
+                        <div className="h-0.5 bg-gray-400"/>
+                        <Link
+                            href="/schedule"
+                            onClick={() => setIsOpen(!isOpen)}
+                            className="pl-2"
+                        >
+                            Schedule
+                        </Link>
+                        <div className="h-0.5 bg-gray-400"/>
+                        <Link
+                            href="/infomation"
+                            onClick={() => setIsOpen(!isOpen)}
+                            className="pl-2"
+                        >
+                            Infomation
+                        </Link>
+                        <div className="h-0.5 bg-gray-400"/>
+                        <Link
+                            href="/map"
+                            onClick={() => setIsOpen(!isOpen)}
+                            className="pl-2"
+                        >
+                            Map
+                        </Link>
+                    </div>
+                </Transition>
+            </nav>
+            <div className="w-full p-6 rounded">blank</div>
+        </>
+    );
 }
